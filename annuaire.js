9835fd98f96590e1599c14e4342d8df5a85d80c0
Simplon-epinal <mlanglois@simplon.co>
2016-04-29 14:43:52 +0200

    Added files via upload

---
+++ annuaire.js
@@ -0,0 +1,12 @@
+
+//Add your code here!!
+//
+//1- Declare your global variables :
+//-the dom elements
+//-your liste of contacts
+//
+//2 - Implement the functions described in your exercice
+//
+//3 - Call the necessary functions to initialize your page content
+//
+
